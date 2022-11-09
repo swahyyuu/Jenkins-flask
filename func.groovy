ba6c3ca6e3870153d21fa323f88c4bc4c865fa9d
swahyu <swahyu1412@gmail.com>
2022-11-09 12:57:31 +0700

    Add current time 1

--- func.groovy
+++ func.groovy
@@ -1,8 +1,14 @@
+import java.text.SimpleDateFormat
+def dateFormat
+def date 
+def formattedDate
+
 def notifyBuild (String buildStatus = 'STARTED') {
    buildStatus = buildStatus ?: 'SUCCESS'
 
+   def dateTime = dateFormatted()
    def colorCode = '#FF0000'
-   def summary = "${buildStatus} : Job Name '${env.JOB_NAME} | Build Number [${env.BUILD_NUMBER}] | URL : ${env.BUILD_URL}'"
+   def summary = "${buildStatus} : Job Name '${env.JOB_NAME} | Build Number [${env.BUILD_NUMBER}] | URL : ${env.BUILD_URL} | Time : ${dateTime}'"
 
    if (buildStatus == 'STARTED') {
       colorCode = '#FFFF00'
@@ -13,5 +19,14 @@ def notifyBuild (String buildStatus = 'STARTED') {
    }
 
    slackSend (color: colorCode, channel: params.CHANNEL_NAME, message: summary)
-   return this
 }
+
+def dateFormatted () {
+  dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+  date = new Date()
+  formattedDate = dateFormat.format(date)
+  return formattedDate
+}
+
+return this
+
